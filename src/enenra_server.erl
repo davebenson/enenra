269bd1d8d006e385ed5b909c08624ebda5f17215
David Benson <david.benson@openx.com>
2019-03-26 11:53:30 -0700

    use RSAPrivateKey directly if it starts that way

--- src/enenra_server.erl
+++ src/enenra_server.erl
@@ -561,5 +561,8 @@ seconds_since_epoch() ->
 compute_signature(PemKeyBin, Msg) ->
     [PemKeyData] = public_key:pem_decode(PemKeyBin),
     PemKey = public_key:pem_entry_decode(PemKeyData),
-    RsaKey = public_key:der_decode('RSAPrivateKey', PemKey#'PrivateKeyInfo'.privateKey),
+    RsaKey = case element(1, PemKey) of
+      'RSAPrivateKey' -> PemKey;
+      _ -> public_key:der_decode('RSAPrivateKey', PemKey#'PrivateKeyInfo'.privateKey)
+    end,
     base64:encode(public_key:sign(Msg, sha256, RsaKey)).
